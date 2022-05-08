50c3ea44e55bbcea4de82b8c4c1617375b484f87
AKA MKII <58581967+AKAMKII6666@users.noreply.github.com>
2022-05-08 16:47:41 +0800

    updated by bobliao

--- tsdx.config.js
+++ tsdx.config.js
@@ -6,19 +6,19 @@ const copy = require('rollup-plugin-copy');
 module.exports = {
   rollup(config, options) {
     config.plugins = [
-      copy({
-        targets: [{ src: './src/img/**/*', dest: './dist/img' }],
-      }),
       postcss({
         inject: true,
         extract: false,
         plugins: [
           url({
-            url: 'rebase', // enable inline assets using base64 encoding
-            maxSize: 10, // maximum file size to inline (in kilobytes)
+            url: 'inline', // enable inline assets using base64 encoding
+            maxSize: 500, // maximum file size to inline (in kilobytes)
             fallback: 'rebase', // fallback method to use if max size is exceeded
           }),
         ],
+      }),
+      copy({
+        targets: [{ src: './src/img/**/*', dest: './dist/img' }],
       }),
     ];
     return config;
